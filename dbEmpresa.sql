8bb312d19217f059f68e20e4719a7fb1d1089760
D0nnye <lucas.sleocadio@senacsp.edu.br>
2022-11-17 16:55:41 -0300

    BDS

--- dbEmpresa.sql
+++ dbEmpresa.sql
@@ -23,40 +23,75 @@ primary key(codUsu),
 foreign key(codFunc)references tbFuncionarios(codFunc)
 );
 
+create table tbFornecedores(
+codForn int not null auto_increment,
+nome varchar(100),
+cnpj varchar(18),
+inscrEst char(15),
+email varchar(100),
+telefone char(10),
+primary key(codForn)
+);
+
 create table tbProdutos(
 codProd int not null auto_increment,
 descricao varchar(100),
 quantidade int default 0 check(quantidade >=0),
 valor decimal (9,2) check(valor >= 0),
-codUsu int not null,
+codForn int not null,
 primary key(codProd),
-foreign key (codUsu) references tbUsuarios(codUsu)
+foreign key(codForn)references tbFornecedores(codForn)
 );
+
+create table tbClientes(
+codCli int not null auto_increment,
+nome varchar (100),
+email varchar(100) not null unique,
+cpf char(14),
+telefone char(10),
+primary key(codCli)
+);
+
+create table tbVendas(
+codVend int not null auto_increment,
+codProd int not null,
+codCli int not null,
+codUsu int not null,
+dataVenda date,
+horaVenda time,
+quantidade int,
+primary key(codVend),
+foreign key(codProd)references tbProdutos(codProd),
+foreign key(codCli)references tbClientes(codCli),
+foreign key(codUsu)references tbUsuarios(codUsu)
+);
+
 
 desc tbFuncionarios;
 desc tbUsuarios;
+desc tbFornecedores;
 desc tbProdutos;
+desc tbClientes;
+desc tbVendas;
+
 
 insert into tbFuncionarios(nome,email,cpf,salario,sexo) values('Jon Jones','jonnyboy@gmail.com','263.658.963-52','2111.00','M');
+
 insert into tbUsuarios(nome,senha,codFunc) values('jon.j','1055050',1);
-insert into tbProdutos(descricao,quantidade,valor,codUsu) values('Sorvete',1,64,1);
 
-insert into tbFuncionarios(nome,email,cpf,salario,sexo) values('Lewis Hamilton','Lew.H@gmail.com','360.964.200-26','7543.00','M');
-insert into tbUsuarios(nome,senha,codFunc) values('lewis.ham','3621063',2);
-insert into tbProdutos(descricao,quantidade,valor,codUsu) values('Frango resfriado',2,70,2);
+insert into tbFornecedores(nome,cnpj,inscrEst,email,telefone)values('Fabricantes LTDA','03.396.1254.0001/36','326.152.654-110','sac@FLTDA.com.br','96332-6910');
 
-insert into tbFuncionarios(nome,email,cpf,salario,sexo) values('Lisa Oliveira','lisa.O@hotmail.com','265.112.093-12','3560.00','F');
-insert into tbUsuarios(nome,senha,codFunc) values('lisa.ol','0369952',3);
-insert into tbProdutos(descricao,quantidade,valor,codUsu) values('Absorvete',1,9,3);
+insert into tbProdutos(descricao,quantidade,valor,codForn) values('Sorvete',1,22.96,1);
 
-insert into tbFuncionarios(nome,email,cpf,salario,sexo) values('Paloma Mesa','palom.me@bol.com.br','186.360.111-24','2630.50','F');
-insert into tbUsuarios(nome,senha,codFunc) values('lisa.ol','6852117',4);
-insert into tbProdutos(descricao,quantidade,valor,codUsu) values('Agua',12,20,4);
+insert into tbClientes(nome,email,cpf,telefone)
+	values('João Sergio do Santos','joao.SS@gmail.com','263.695.785-23','96524-8599');
 
-insert into tbFuncionarios(nome,email,cpf,salario,sexo) values('Ryan Alves Cunha','RAC@gmail.com','036.314.260-90','1200.50','M');
-insert into tbUsuarios(nome,senha,codFunc) values('ryan.ac','0396226',5);
-insert into tbProdutos(descricao,quantidade,valor,codUsu) values('Grana Padano',1,1850,5);
+insert into tbVendas(codProd,codCli,codUsu,dataVenda,horaVenda,quantidade)values(1,1,1,'2022/01/09','15:04:12',12);
+
 
 select * from tbFuncionarios;
 select * from tbUsuarios;
+select * from tbFornecedores;
 select * from tbProdutos;
+select * from tbClientes;
+select * from tbVendas;
